7945d1b0beb96254e8a5734d0160ff5bc33f31c7
felipmiguel <fmiguel@microsoft.com>
2023-05-04 22:24:39 +0200

    Namespaces rename to align to Azure SDK

---
+++ Microsoft.Azure.Data.Extensions.NpgsqlTests/PostgresConnectionTests.cs
@@ -0,0 +1,78 @@
+﻿using Azure.Identity;
+using Microsoft.Azure.Data.Extensions.Npgsql;
+using Microsoft.Azure.Data.Extensions.NpgsqlTests.Utils;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
+using Sample.Repository;
+using System.Runtime.CompilerServices;
+
+namespace Microsoft.Azure.Data.Extensions.NpgsqlTests
+{
+    [TestClass]
+    public class PostgresConnectionTests
+    {
+        private static IConfiguration? configuration;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+        }
+
+        private static async Task ValidateDataSourceAsync(NpgsqlDataSource dataSource)
+        {
+            Assert.IsNotNull(dataSource);
+            using NpgsqlConnection connection = await dataSource.OpenConnectionAsync();
+            using NpgsqlCommand cmd = connection.CreateCommand();
+            cmd.CommandText = "SELECT now()";
+            DateTime? serverDate = (DateTime?)await cmd.ExecuteScalarAsync();
+            Assert.IsNotNull(serverDate);
+        }
+
+        [TestMethod]
+        public async Task NoExtensionDefaultAzureCredential()
+        {
+            Assert.IsNotNull(configuration);
+            TokenCredentialNpgsqlPasswordProvider passwordProvider = new TokenCredentialNpgsqlPasswordProvider(new DefaultAzureCredential());
+            // Connection string does not contain password
+            NpgsqlDataSourceBuilder dataSourceBuilder = new NpgsqlDataSourceBuilder(configuration.GetConnectionString());
+            NpgsqlDataSource dataSource = dataSourceBuilder
+                            .UsePeriodicPasswordProvider(passwordProvider.PasswordProvider, TimeSpan.FromMinutes(2), TimeSpan.FromMilliseconds(100))
+                            .Build();
+            await ValidateDataSourceAsync(dataSource);
+        }
+
+        [TestCategory("server-only")]
+        [TestMethod]
+        public async Task NoExtensionConstructorWithManagedIdentity()
+        {
+            Assert.IsNotNull(configuration);
+            string? managedIdentityClientId = configuration.GetManagedIdentityClientId();
+            Assert.IsNotNull(managedIdentityClientId);
+            TokenCredentialNpgsqlPasswordProvider passwordProvider = new TokenCredentialNpgsqlPasswordProvider(new DefaultAzureCredential(new DefaultAzureCredentialOptions { ManagedIdentityClientId = managedIdentityClientId }));
+            NpgsqlDataSourceBuilder dataSourceBuilder = new NpgsqlDataSourceBuilder(configuration.GetConnectionString());
+            NpgsqlDataSource dataSource = dataSourceBuilder
+                            .UsePeriodicPasswordProvider(passwordProvider.PasswordProvider, TimeSpan.FromMinutes(2), TimeSpan.FromMilliseconds(100))
+                            .Build();
+            await ValidateDataSourceAsync(dataSource);
+        }
+
+        [TestCategory("local-only")]
+        [TestMethod]
+        public async Task NoExtensionConstructorWithTokenCredential()
+        {
+            Assert.IsNotNull(configuration);
+            AzureCliCredential credential = new AzureCliCredential();
+            TokenCredentialNpgsqlPasswordProvider passwordProvider = new TokenCredentialNpgsqlPasswordProvider(credential);
+            NpgsqlDataSourceBuilder dataSourceBuilder = new NpgsqlDataSourceBuilder(configuration.GetConnectionString());
+            NpgsqlDataSource dataSource = dataSourceBuilder
+                            .UsePeriodicPasswordProvider(passwordProvider.PasswordProvider, TimeSpan.FromMinutes(2), TimeSpan.FromMilliseconds(100))
+                            .Build();
+            await ValidateDataSourceAsync(dataSource);
+        }
+    }
+}
---
+++ Microsoft.Azure.Data.Extensions.NpgsqlTests/Utils/ConfigurationExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.Data.Extensions.NpgsqlTests.Utils
+{
+    static internal class ConfigurationExtensions
+    {
+        public static string GetConnectionString(this IConfiguration configuration)
+        {
+            Assert.IsNotNull(configuration);
+            NpgsqlConnectionStringBuilder connectionStringBuilder = new NpgsqlConnectionStringBuilder
+            {
+                Host = configuration.GetSection("postgresql:host").Value,
+                Database = configuration.GetSection("postgresql:database").Value,
+                Username = configuration.GetSection("postgresql:user").Value,
+                Port = 5432,
+                SslMode = SslMode.Require,
+                TrustServerCertificate = true,
+                Timeout = 30
+            };
+            return connectionStringBuilder.ConnectionString;
+        }
+
+        public static string? GetManagedIdentityClientId(this IConfiguration configuration)
+        {
+            return configuration?.GetSection("msi")?.Value;
+        }
+    }
+}
