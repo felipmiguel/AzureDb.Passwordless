fae7b21c121d1286ac555387df86c50ad50bbd78
fmiguel <fmiguel@outlook.com>
2023-07-17 21:58:35 +0200

    scale down mysql

--- iac/mysql/main.tf
+++ iac/mysql/main.tf
@@ -60,7 +60,7 @@ resource "azurerm_mysql_flexible_server" "database" {
   administrator_login    = var.administrator_login
   administrator_password = random_password.password.result
 
-  sku_name                     = "B_Standard_B1ms"
+  sku_name                     = "B_Standard_B1s"
   version                      = "8.0.21"
   backup_retention_days        = 7
   geo_redundant_backup_enabled = false
