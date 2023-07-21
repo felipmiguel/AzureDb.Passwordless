921c5de2a29eb4ed4c1428cf3b89afdfba50a141
fmiguel <fmiguel@outlook.com>
2023-07-22 00:56:02 +0200

    EF Tests refactored

--- Microsoft.Azure.Data.Extensions.Npgsql.EntityFrameworkCore/tests/DbContextOptionsBuilderTests.cs
+++ Microsoft.Azure.Data.Extensions.Npgsql.EntityFrameworkCore/tests/DbContextOptionsBuilderTests.cs
@@ -27,21 +27,6 @@ namespace Microsoft.Azure.Data.Extensions.Npgsql.EntityFrameworkCore.Tests
 
             var serviceProvider = services.BuildServiceProvider();
             await ChecklistContextValidator.ValidateChecklistAsync(serviceProvider);
-            //var contextFactory = serviceProvider.GetRequiredService<IDbContextFactory<ChecklistContext>>();
-            //using var dbContext = await contextFactory.CreateDbContextAsync();
-            //await dbContext.Database.OpenConnectionAsync();
-            //var chk = await dbContext.Checklists.AddAsync(new Checklist
-            //{
-            //    Date = DateTime.UtcNow,
-            //    Description = "Test sample item",
-            //    Name = "Test Item"
-            //});
-            //await dbContext.SaveChangesAsync();
-
-            //var chk2 = await dbContext.Checklists.FindAsync(chk.Entity.ID);
-            //Assert.IsNotNull(chk2);
-            //dbContext.Checklists.Remove(chk2);
-            //await dbContext.SaveChangesAsync();
         }
     }
 }
--- Microsoft.Azure.Data.Extensions.Pomelo.EntityFrameworkCore/tests/DbContextOptionsBuilderTests.cs
+++ Microsoft.Azure.Data.Extensions.Pomelo.EntityFrameworkCore/tests/DbContextOptionsBuilderTests.cs
@@ -31,24 +31,7 @@ namespace Microsoft.Azure.Data.Extensions.Pomelo.EntityFrameworkCore.Tests
             });
 
             var serviceProvider = services.BuildServiceProvider();
-            await ChecklistContextValidator.ValidateChecklistAsync(serviceProvider);
-            //var contextFactory = serviceProvider.GetRequiredService<IDbContextFactory<ChecklistContext>>();
-            //using var dbContext = await contextFactory.CreateDbContextAsync();
-            //await dbContext.Database.OpenConnectionAsync();
-            //var chk = await dbContext.Checklists.AddAsync(new Checklist
-            //{
-            //    Date = DateTime.Now,
-            //    Description = "Test sample item",
-            //    Name = "Test Item"
-            //});
-            //await dbContext.SaveChangesAsync();
-
-            //var chk2 = await dbContext.Checklists.FindAsync(chk.Entity.ID);
-            //Assert.IsNotNull(chk2);
-            //dbContext.Checklists.Remove(chk2);
-            //await dbContext.SaveChangesAsync();
-
-            
+            await ChecklistContextValidator.ValidateChecklistAsync(serviceProvider);            
         }
     }
 }
